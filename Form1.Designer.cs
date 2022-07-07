850fac4416afd355081a4085f9a03f901977701c
bas1c <68533770+bas1c1@users.noreply.github.com>
2022-07-07 16:37:53 +0300

    HotKeyEditor

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace HotKeyEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.EnableAutoDragDrop = true;
+            this.richTextBox2.Font = new System.Drawing.Font("Sitka Small", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.richTextBox2.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(762, 456);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            //this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(762, 456);
+            this.Controls.Add(this.richTextBox2);
+            this.Font = new System.Drawing.Font("Montserrat Subrayada", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Empty";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace HotKeyEditor
+{
+    public partial class Form1 : Form
+    {
+        public string fname = string.Empty;
+        public Form1()
+        {
+            
+            InitializeComponent();
+            richTextBox2.PreviewKeyDown += OnPreviewKeyDown;
+            richTextBox2.KeyDown += OnKeyDown;
+        }
+
+        private void OnPreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.Control)
+            {
+                
+                e.IsInputKey = true;
+            }
+        }
+
+        private void richTextBox2_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                e.Effect = DragDropEffects.Move;
+                richTextBox2.Text = e.Data.GetData(DataFormats.Text).ToString();
+            }
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.S)
+            {
+                if (fname != string.Empty)
+                {
+                    File.WriteAllText(fname, richTextBox2.Text);
+                }
+                else
+                {
+                    saveFileDialog1.FileName = "new";
+                    saveFileDialog1.ShowDialog();
+                    fname = saveFileDialog1.FileName;
+                    ActiveForm.Text = fname;
+                    if (saveFileDialog1.DefaultExt != string.Empty && fname != string.Empty)
+                    {
+                        File.WriteAllText(fname, richTextBox2.Text);
+                    }
+                    else
+                    {
+                        ActiveForm.Text = "Empty";
+                        fname = string.Empty;
+                    }
+                }
+            }
+
+            else if (e.Control && e.KeyCode == Keys.F)
+            {
+                fontDialog1.ShowDialog();
+                richTextBox2.Font = fontDialog1.Font;
+            }
+
+            else if (e.Control && e.KeyCode == Keys.D)
+            {
+                openFileDialog1.FileName = string.Empty;
+                openFileDialog1.ShowDialog();
+                fname = openFileDialog1.FileName;
+                ActiveForm.Text = "Empty";
+
+
+                if(fname != string.Empty)
+                    File.Delete(fname);
+                fname = string.Empty;
+            }
+
+            else if (e.Control && e.KeyCode == Keys.S && e.Alt)
+            {
+                string text = string.Empty;
+                text +=  "Font: " + richTextBox2.Font.Name + '\n';
+                text += '\n' + "FontSize: " + richTextBox2.Font.Size + '\n';
+                text += '\n' + "FontStyle: " + richTextBox2.Font.Style + '\n';
+                text += '\n' + "ForeColor: " + richTextBox2.ForeColor.ToString() + '\n';
+                text += '\n' + "BackColor: " + richTextBox2.BackColor.ToString();
+                File.WriteAllText("style.cfg", text);
+            }
+
+            else if (e.Control && e.KeyCode == Keys.R)
+            {
+                colorDialog1.ShowDialog();
+                richTextBox2.ForeColor = colorDialog1.Color;
+            }
+
+            else if (e.Control && e.KeyCode == Keys.G)
+            {
+                colorDialog1.ShowDialog();
+                richTextBox2.BackColor = colorDialog1.Color;
+            }
+
+            else if (e.Control && e.Shift && e.KeyCode == Keys.S)
+            {
+                saveFileDialog1.FileName = string.Empty;
+                saveFileDialog1.ShowDialog();
+                fname = saveFileDialog1.FileName;
+                ActiveForm.Text = fname;
+                if (saveFileDialog1.DefaultExt != string.Empty && fname != string.Empty)
+                {
+                    File.WriteAllText(fname, richTextBox2.Text);
+                }
+            }
+
+            else if (e.Control && e.KeyCode == Keys.O)
+            {
+                openFileDialog1.FileName = string.Empty;
+                openFileDialog1.ShowDialog();
+                fname = openFileDialog1.FileName;
+                ActiveForm.Text = fname;
+                if (fname != string.Empty)
+                    richTextBox2.Text = File.ReadAllText(fname);
+                else
+                {
+                    ActiveForm.Text = "Empty";
+                    fname = string.Empty;
+                }
+            }
+
+            else if (e.Control && e.Shift && e.KeyCode == Keys.N)
+            {
+                saveFileDialog1.FileName = string.Empty;
+                saveFileDialog1.ShowDialog();
+                fname = saveFileDialog1.FileName;
+                ActiveForm.Text = fname;
+                if (fname != string.Empty)
+                {
+                    File.WriteAllText(fname, string.Empty);
+                    richTextBox2.Text = File.ReadAllText(fname);
+                } 
+                else
+                {
+                    ActiveForm.Text = "Empty";
+                    fname = string.Empty;
+                }
+            }
+
+            else if (e.Control && e.KeyCode == Keys.P)
+            {
+                openFileDialog1.FileName = string.Empty;
+                openFileDialog1.ShowDialog();
+                fname = openFileDialog1.FileName;
+                ActiveForm.Text = fname;
+                System.Drawing.Printing.PrintDocument document = new System.Drawing.Printing.PrintDocument();
+                document.DocumentName = fname;
+                printDialog1.Document = document;
+                printDialog1.ShowDialog();
+            }
+
+            else if (e.Control && e.KeyCode == Keys.N)
+            {
+                if (System.Diagnostics.Process.Start("HotKeyEditor.exe") == null)
+                    Application.Exit();
+            }
+        }
+    }
+}
